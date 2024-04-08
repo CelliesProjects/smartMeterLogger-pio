18d6861e4f14f028eb36b0a74fd563d65896cd9e
Cellie <CelliesProjects@users.noreply.github.com>
2024-04-08 12:35:07 +0200

    Set static ip to 192.168.0.90

--- src/setup.h
+++ src/setup.h
@@ -18,7 +18,7 @@ const uint16_t WS_BRIDGE_PORT = 80;          /* bridge port */
 /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
 #define SET_STATIC_IP true
 
-const IPAddress STATIC_IP(192, 168, 0, 91);   /* This should be outside your router dhcp range! */
+const IPAddress STATIC_IP(192, 168, 0, 90);   /* This should be outside your router dhcp range! */
 const IPAddress GATEWAY(192, 168, 0, 1);      /* Set to your gateway ip address */
 const IPAddress SUBNET(255, 255, 255, 0);     /* Usually 255,255,255,0 check in your router or pc connected to the same network */
 const IPAddress PRIMARY_DNS(192, 168, 0, 10); /* Check in your router */
